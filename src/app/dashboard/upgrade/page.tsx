a0573dbd65d3c9c9ecbc7a6c76a90c62f76c1270
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2024-08-07 09:35:57 +0530

    upgrade page added

--- src/app/dashboard/upgrade/page.tsx
+++ src/app/dashboard/upgrade/page.tsx
@@ -1,9 +1,53 @@
+"use client"
+
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import { Check } from 'lucide-react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+
 
 const Upgrade = () => {
+  const router = useRouter()
+  const handleOnClick = async () => {
+    const response = await axios.post('/api/upgrade/checkout')
+    // push user to the stripe url
+  }
   return (
-    <div>
-      Upgrade
+    <div className='mx-5 py-2'>
+      <div className='mt-5 py-6 bg-white rounded'>
+        <h2 className='font-medium'>Upgrade Credit</h2>
+      </div>
+      <div className='mt-5 py-6 px-4 rounded'>
+        <Card className="w-[350px] flex flex-col mx-auto">
+          <CardHeader>
+            <CardTitle>$10 One-Time Purchase</CardTitle>
+            <CardDescription>10,000 AI Credit</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className=''>
+              <p className='flex my-2 gap-2'>
+                <Check />100,000 words/purchase
+              </p>
+              <p className='flex my-2 gap-2'>
+                <Check />All Template Access
+              </p>
+              <p className='flex my-2 gap-2'>
+                <Check />Retain All History
+              </p>
+            </div>
+            <Button  className='mt-5' onClick={handleOnClick}>Purchase</Button>
+          </CardContent>
+          
+        </Card>
+      </div>
     </div>
   )
 }
