4909c4feaaf471abf11c35866d5245d281b6407e
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2024-08-10 00:30:46 +0530

    shadcn Chart added

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import Sidebar from './_components/Sidebar'
+import React from "react";
+import { AIUsage } from "./_components/ai-usage";
+import Sidebar from "./_components/Sidebar";
 
-const DashboardLayout = ({ children }: Readonly<{children: React.ReactNode}>) => {
+const DashboardLayout = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div className='bg-gray-50 h-screen'>
-        <div className='md:w-64 hidden md:block fixed'>
-            <Sidebar />
-        </div>
-        <div className='md:ml-64 bg-gray-50 h-fit pb-5'>
-            {children}
-        </div>
-    </div>
-  )
-}
+    <div className="bg-gray-50 h-screen">
 
-export default DashboardLayout
+      <div className="md:w-64 hidden md:flex flex-col fixed h-full overflow-y-auto">
+        <Sidebar />
+        <div className="mt-6"> 
+          <AIUsage />
+        </div>
+      </div>
+
+      <div className="md:ml-64 bg-gray-50 h-fit pb-5">{children}</div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
